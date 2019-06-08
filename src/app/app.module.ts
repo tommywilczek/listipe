0f648f07dc3d4829e22483322a65a93cab1d5272
Tommy Wilczk <tommywilczek@utexas.edu>
2019-06-08 17:08:35 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipeListComponent } from './recipeComponents/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipeComponents/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipeComponents/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './shoppingComponents/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shoppingComponents/shopping-list-edit/shopping-list-edit.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipeComponents/recipes/recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    HeaderComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule,
