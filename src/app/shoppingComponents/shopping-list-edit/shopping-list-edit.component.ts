20228b83cfcf3e6773f227bdf7bd9a3bbc149894
Tommy Wilczk <tommywilczek@utexas.edu>
2019-08-27 12:02:12 -0500

    Added Template-Driven forms to list edit component

--- src/app/shoppingComponents/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shoppingComponents/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, ElementRef, ViewChild, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from 'src/app/services/shoppingListService/shopping-list.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-list-edit',
@@ -8,18 +9,15 @@ import { ShoppingListService } from 'src/app/services/shoppingListService/shoppi
   styleUrls: ['./shopping-list-edit.component.css']
 })
 export class ShoppingListEditComponent implements OnInit {
-  @ViewChild('nameInput', {static: false}) nameInputRef: ElementRef;
-  @ViewChild('amountInput', {static: false}) amountInputRef: ElementRef;
 
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
   }
 
-  onSubmitItem() {
-    const newIngName = this.nameInputRef.nativeElement.value;
-    const newIngAmount = this.amountInputRef.nativeElement.value;
-    const newIngredient = new Ingredient(newIngName, newIngAmount);
+  onAddItem(form: NgForm) {
+    const formValue = form.value;
+    const newIngredient = new Ingredient(formValue.name, formValue.amount);
     this.shoppingListService.addIngredient(newIngredient);
   }
 
